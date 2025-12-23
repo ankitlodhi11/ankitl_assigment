4cf1e1b51d74be28cf297e78681d552a38a85242
dev-new-code <ankitlodhi11@gmail.com>
2025-12-23 14:47:36 +0530

    Update providers.tf

--- environment/dev/providers.tf
+++ environment/dev/providers.tf
@@ -2,24 +2,18 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.32.0"
+      version = "4.49.0"
     }
   }
 backend "azurerm" {
-  storage_account_name = "tfstate2025abc123"
-  container_name       = "tfstate"
-  key                  = "dev/terraform.tfstate"
-  use_azuread_auth     = true
-  client_id            = "91e9bddf-55ad-4819-ad87-eef6df4ac07e"
-  client_secret        = "vM38Q~Z9mnCv9cKQvOPLKOtTow4eAaUc2hm4mc-H"
-  tenant_id            = "7a411d05-7587-426e-a2d7-5a3b50200561"
+resource_group_name  = "rg-pilu-dev-todoapp-01"
+storage_account_name = "vmstorageal"
+container_name       = "tfstate"
+key                  = "dev.terraform.tfstate"
 }
 }
+
 provider "azurerm" {
   features {}
-  subscription_id = "56de03ab-1570-40b1-bd13-5a9ca5b1e106"  # Resources ke liye correct subscription
+  subscription_id = "56de03ab-1570-40b1-bd13-5a9ca5b1e106"
 }
-
-
-
-
